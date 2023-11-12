504b33853f41dfe534fdf09e324a16ee256a1c0f
Bliod-Cook <Bliod_Cook@outlook.com>
2023-11-12 15:25:29 +0800

    🐎 ci: update

--- src/update.py
+++ src/update.py
@@ -4,7 +4,7 @@ from PySide6 import QtCore, QtWidgets,QtGui
 
 if __name__ == "__main__":
     req = requests.get("https://github.cooluc.com/https://github.com/Jaffrez\
-                            /seewo_tools/releases/download/master/seewo_tools_setup.exe")
+                            /seewo_tools/releases/download/master/main.exe")
     if not req.status_code == 200:
         QtWidgets.QMessageBox(None,"更新失败", "更新失败")
         os.execl("./main.exe", "main.exe","")
