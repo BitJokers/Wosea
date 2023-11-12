63b34d35475a86c5348d24e9063046b333d09277
Bliod-Cook <Bliod_Cook@outlook.com>
2023-11-12 13:54:20 +0800

    🐞 fix: 更改更新url

--- src/update.py
+++ src/update.py
@@ -3,6 +3,6 @@ import os
 
 if __name__ == "__main__":
     file = open("./main.exe", 'wb')
-    file.write(requests.get("***").content)
+    file.write(requests.get("https://github.cooluc.com/https://github.com/Jaffrez/seewo_tools/releases/download/master/seewo_tools_setup.exe").content)
     file.close()
     os.execl("./main.exe", "main.exe","")
