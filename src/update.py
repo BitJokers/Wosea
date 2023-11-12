9fcdc632bdf01169b1e1cb09ab51b06630c497e5
Jaffrez <Jaffrez@outlook.com>
2023-11-12 13:58:47 +0800

    🌈 style(update.py):

--- src/update.py
+++ src/update.py
@@ -3,6 +3,7 @@ import os
 
 if __name__ == "__main__":
     file = open("./main.exe", 'wb')
-    file.write(requests.get("https://github.cooluc.com/https://github.com/Jaffrez/seewo_tools/releases/download/master/seewo_tools_setup.exe").content)
+    file.write(requests.get("https://github.cooluc.com/https://github.com/Jaffrez\
+                            /seewo_tools/releases/download/master/seewo_tools_setup.exe").content)
     file.close()
     os.execl("./main.exe", "main.exe","")
