b1463406a2c842ee43521f935f954af46e978a59
Jaffrez <Jaffrez@outlook.com>
2023-11-16 22:57:36 +0800

    ✨ feat(numGen): 绑定学号生成按钮

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 
 from PySide6 import QtGui,QtWidgets,QtCore
 import sys
-
 import PySide6.QtCore
 import PySide6.QtWidgets
 from mainWindow import Ui_MainWindow
+import numGen
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -12,6 +12,9 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui.genNumButton.clicked.connect(numGen.start)
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
--- src/modules/Update/update.py
+++
@@ -1,8 +0,0 @@
-import wget
-import os
-from PySide6 import QtWidgets
-
-if __name__:
-    url = "https://github.cooluc.com/https://github.com/Jaffrez/seewo_tools/releases/download/master/main.exe"
-    wget.download(url,'.\main.exe')
-    os.execl("./main.exe", "main.exe","")
