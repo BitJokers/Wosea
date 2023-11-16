79d769b6b4d9697d93c37a67789ee66cc3c57195
Bliod-Cook <Bliod_Cook@outlook.com>
2023-11-16 17:53:03 +0800

    🐳 chore:

--- src/modules/Update/update.py
+++ src/modules/Update/update.py
@@ -1,13 +1,8 @@
-import requests
+import wget
 import os
 from PySide6 import QtWidgets
 
 if __name__:
-    req = requests.get("https://github.cooluc.com/https://github.com/Jaffrez/seewo_tools/releases/download/master/main.exe")
-    if not req.status_code == 200:
-        QtWidgets.QMessageBox(None,"更新失败", "更新失败")
-        os.execl("./main.exe", "main.exe","")
-    file = open("./main.exe", 'wb')
-    file.write()
-    file.close()
+    url = "https://github.cooluc.com/https://github.com/Jaffrez/seewo_tools/releases/download/master/main.exe"
+    wget.download(url,'.\main.exe')
     os.execl("./main.exe", "main.exe","")
