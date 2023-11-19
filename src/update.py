f0113087ce57996588815cbbb3cb569522d42217
Jaffrez <Jaffrez@outlook.com>
2023-11-19 15:24:59 +0800

    📃 docs(ALL): 添加文件头

--- src/update.py
+++ src/update.py
@@ -1,3 +1,13 @@
+'''
+Author: Jaffrez Jaffrez@outlook.com
+Date: 2023-11-16 22:30:37
+LastEditors: Jaffrez Jaffrez@outlook.com
+LastEditTime: 2023-11-19 15:22:49
+FilePath: /Wosea/src/update.py
+Description: 更新模块
+Copyright (c) 2023 by Jaffrez, All Rights Reserved.
+'''
+
 import wget
 import os
 
